1cd9ed65d0cf62b2a778699a4fc545ba3b3ae741
andreyrsy <contatomainort@gmail.com>
2026-01-14 20:29:12 -0300

    fix: correct Phosphor Icons names to match reference (add Icon suffix)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react"
+import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
 
 
 function Footer() {
@@ -14,9 +14,9 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <LinkedinLogoIcon size={48} weight='bold' />
+                        <InstagramLogoIcon size={48} weight='bold' />
+                        <FacebookLogoIcon size={48} weight='bold' />
                     </div>
                 </div>
             </div>
