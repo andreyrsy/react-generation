c3da9a8d3afa9891c53b49b142751f74a965dc91
andreyrsy <contatomainort@gmail.com>
2026-01-16 14:36:40 -0300

    Revert "feat: add user info to navbar (personalization)"
    This reverts commit 65211fb98bc61bfff906d196120c8a2a90ed81a6.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,14 +3,13 @@ import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
 
-
 function Navbar() {
 
 
     const navigate = useNavigate();
 
 
-    const { usuario, handleLogout } = useContext(AuthContext)
+    const { handleLogout } = useContext(AuthContext)
 
 
     function logout() {
@@ -27,16 +26,9 @@ function Navbar() {
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
             
-                <div className="container flex justify-between text-lg mx-8 items-center">
+                <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-                    <div className="flex gap-2 items-center">
-                        <img src={usuario.foto} alt="Foto do Usuário" className="w-12 h-12 rounded-full" />
-                        <div className="flex flex-col text-sm">
-                            <p>Bem vindo!</p>
-                            <p className="font-bold">{usuario.nome}</p>
-                        </div>
-                    </div>
 
                     <div className='flex gap-4'>
                         Postagens
@@ -50,7 +42,6 @@ function Navbar() {
         </>
     )
 }
-
 
 
 export default Navbar
